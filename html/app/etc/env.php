b790938aa4874713049c1c53901a003c75b3d21e
shashi <s.desilva@ism-apac.com>
2017-01-20 15:55:48 +0530

    First commit

---
+++ html/app/etc/env.php
@@ -0,0 +1,68 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'db' => 
+  array (
+    'connection' => 
+    array (
+      'indexer' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2localdb',
+        'username' => 'root',
+        'password' => 'root123',
+        'active' => '1',
+        'persistent' => NULL,
+      ),
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2localdb',
+        'username' => 'root',
+        'password' => 'root123',
+        'active' => '1',
+      ),
+    ),
+    'table_prefix' => '',
+  ),
+  'install' => 
+  array (
+    'date' => 'Thu, 28 Jul 2016 11:13:49 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '9d5a29b858153f4c4cde1ebed6939fd1',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'target_rule' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+);
